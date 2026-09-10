ac8a0c2045b9714ec14faed4bebb8c87ed544d1e
JonathanLv <63095506+XiaolongLv@users.noreply.github.com>
2026-09-10 10:14:56 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -25,7 +25,7 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://jonathanlv.com' },
-    { title: '', url: '' }
+    { title: '一个想要改变世界的普通人', url: '' }
   ],
 
   // 英雄区左右侧组件颠倒位置
